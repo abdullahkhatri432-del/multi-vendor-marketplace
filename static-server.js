eb34b02a91f5d55c4bd0a693ca59bb9e2abb3187
abdullahkhatri432-del <abdullahkhatri432@gmail.com>
2026-08-10 18:17:43 +0530

    fix(static-server): remove duplicate server declarations, add proper error handling

--- static-server.js
+++ static-server.js
@@ -2,103 +2,25 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const server = http.createServer((req, res) => {
-  let filePath = '.' + req.url;
-  if (filePath === './') filePath = './index.html';
-  filePath = path.join(__dirname, 'dist', filePath);
-  
-  const ext = path.extname(filePath);
-  const contentType = {
-    '.html': 'text/html',
-    '.js': 'application/javascript',
-    '.css': 'text/css',
-    '.json': 'application/json',
-    '.png': 'image/png',
-    '.jpg': 'image/jpeg',
-    '.svg': 'image/svg+xml',
-    '.ico': 'image/x-icon'
-  }[ext] || 'text/plain';
-  
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      if (err.code === 'ENOENT') {
-        // SPA fallback to index.html
-        fs.readFile(path.join(__dirname, 'dist', 'index.html'), (err, data) => {
-          if (err) {
-            res.writeHead(500);
-            res.end('Server Error');
-          } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(data);
-          }
-        });
-      } else {
-        res.writeHead(200, { 'Content-Type': contentType });
-        res.end(data);
-      }
-    });
-  });
+const contentType = {
+  '.html': 'text/html',
+  '.js': 'application/javascript',
+  '.css': 'text/css',
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.svg': 'image/svg+xml',
+  '.ico': 'image/x-icon'
 };
 
 const server = http.createServer((req, res) => {
   let filePath = '.' + req.url;
   if (filePath === './') filePath = './index.html';
   filePath = path.join(__dirname, 'dist', filePath);
-  
-  const ext = path.extname(filePath);
-  const contentType = {
-    '.html': 'text/html',
-    '.js': 'application/javascript',
-    '.css': 'text/css',
-    '.json': 'application/json',
-    '.png': 'image/png',
-    '.jpg': 'image/jpeg',
-    '.svg': 'image/svg+xml',
-    '.ico': 'image/x-icon'
-  }[path.extname(filePath)] || 'text/plain';
-  
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      if (err.code === 'ENOENT') {
-        // SPA fallback to index.html
-        fs.readFile(path.join(__dirname, 'dist', 'index.html'), (err, data) => {
-          if (err) {
-            res.writeHead(500);
-            res.end('Server Error');
-          } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(data);
-          }
-        });
-      } else {
-        res.writeHead(200, { 'Content-Type': contentType });
-        res.end(data);
-      }
-    });
-  });
-};
 
-const server = http.createServer((req, res) => {
-  let filePath = '.' + req.url;
-  if (filePath === './') filePath = './index.html';
-  filePath = path.join(__dirname, 'dist', filePath);
-  
-  const ext = path.extname(filePath);
-  const contentType = {
-    '.html': 'text/html',
-    '.js': 'application/javascript',
-    '.css': 'text/css',
-    '.json': 'application/json',
-    '.png': 'image/png',
-    '.jpg': 'image/jpeg',
-    '.svg': 'image/svg+xml',
-    '.ico': 'image/x-icon'
-  }[path.extname(filePath)] || 'text/plain';
-  
   fs.readFile(filePath, (err, data) => {
     if (err) {
       if (err.code === 'ENOENT') {
-        // SPA fallback to index.html
         fs.readFile(path.join(__dirname, 'dist', 'index.html'), (err, data) => {
           if (err) {
             res.writeHead(500);
@@ -109,11 +31,14 @@ const server = http.createServer((req, res) => {
           }
         });
       } else {
-        res.writeHead(200, { 'Content-Type': contentType });
-        res.end(data);
+        res.writeHead(500);
+        res.end('Server Error');
       }
-    });
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType[path.extname(filePath)] || 'text/plain' });
+      res.end(data);
+    }
   });
-};
+});
 
 server.listen(5173, () => console.log('Static server running on http://localhost:5173'));
